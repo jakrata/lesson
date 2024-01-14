f9b4c7996bc3b62f263c5c1337d962b993bfe079
Igor Fink <igorfink186@gmail.com>
2024-01-14 13:45:33 +0500

    main lesson 2

--- lesson_main/script.js
+++ lesson_main/script.js
@@ -1,6 +1,16 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let misson;
-let period;
+let money = 90000; //Доход за месяц
+let income = '10000'; //Дополнительный доход 
+let addExpenses = '25000'; //Расходы
+let deposit = false; //Булевое значение
+let misson = 1000000; //Какую сумму накопить
+let period = 24; //Период в месяцах
+let budgetDay = money / 30; //Дневной бюджет
+
+console.log(typeof money); //Вывод тип данных 
+console.log(typeof income); //Вывод тип данных 
+console.log(typeof deposit); //Вывод тип данных 
+console.log(addExpenses.length); //Вывод длинны строки
+console.log('Период равен ' + period + ' месяца');
+console.log('Цель заработать ' + misson + ' рублей');
+console.log(addExpenses.toLocaleLowerCase().split(', ')); //Перевод в нижний регистр, разбил строку на массив
+console.log(budgetDay); //Вывод новой переменной
